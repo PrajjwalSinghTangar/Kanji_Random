8874f6c8a911c0ea1fb20f031ccb0863e3edc3b3
prajjwalsinghtangar <prajjwalsinghtangar@gmail.com>
2022-09-06 21:08:05 +0530

    adding heroku settings

--- kanji_main/settings.py
+++ kanji_main/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+#r4g^@02)ubd-)4aygn6sujz#8pl9ll8ymp4wadlf=3@z00@^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['randomkanji.herokuapp.com','127.0.0.1']
 
 
 # Application definition
